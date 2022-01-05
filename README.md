af8b29d1fc050b6a631ba76cbc413fb0f6fb5254
Tianxiang Lan <tlan@ksmpartners.com>
2022-01-05 09:17:54 -0500

    second trans prob 1 completed

--- README.md
+++ README.md
@@ -5,8 +5,16 @@
 - script_second_transformation prob 2 outputs to /Output_Graphs/top_ten_severe_events_states.jepg
 - script_second_transformation prob 3 outputs to /Output_Graphs/2nd Trans Prob 3
 
+#### Output_Graphs:
+- contains the graphs outputted by our scripts: ergo the outputs of our POC
+
 #### Jupyter Notebook Notes:
 - if you'd like to see the notebooks for how the script works, please use the JN - 'Title'.ipynb files
+
+#### raw_and_cleaned_data
+- 3 partitions are the 3 datasets from the original kaggle dataset
+- Output Csvs are the csvs that were outputted from the jupyter noteoboks
+- Output Csvs Script Tests are the csvs that came out from the scripts
 
 Raw data can be found here: 
 https://www.kaggle.com/sobhanmoosavi/us-weather-events
--- script_second_transformation.py
+++ script_second_transformation.py
@@ -1,10 +1,99 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
-problem_1_csv = ''
+problem_1_csv = 'raw_and_cleaned_data/Output Csvs/types_by_latitude_by_month.csv'
 problem_2_3_csv = 'raw_and_cleaned_data/Output Csvs Script Tests/severe_events.csv'
 
-colors = {'Snow': 'white', 'Fog': 'gray', 'Storm': 'red', 'Rain': 'green', 'Cold': 'blue', 'Precipitation': 'black', 'Hail': 'yellow'}
+colors = {'Cold': 'lightblue', 'Fog': 'gray', 'Storm': 
+              'red', 'Rain': 'lightgreen', 'Hail': 'yellow', 
+              'Precipitation': 'black', 'Snow': 'white'}
+
+weather_types = ['Cold', 'Fog', 'Storm', 'Rain', 'Hail', 'Precipitation', 'Snow']
+
+
+def color_matcher(str_col):
+    first_word_value = str_col.split()[0][:-1]
+    if first_word_value in weather_types:
+        return colors[first_word_value]
+
+def prob1_graph():
+    types_lat_month_df = pd.read_csv(problem_1_csv, index_col = [0])
+    types_lat_month_df.fillna(value = 0, inplace = True)
+
+    types_lat_month_df = types_lat_month_df[['Month', 'Lat Decile Rank', 'Lat Decile', 'Cold', 'Fog', 'Hail', 'Precipitation', 'Rain', 'Snow']]
+    months = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: "Apr", 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+
+    types_lat_month_df = types_lat_month_df.replace({'Month': months})
+
+    types_lat_month_df[['Cold','Fog', 'Hail', 'Precipitation', 'Rain', 'Snow']] = types_lat_month_df[['Cold','Fog', 'Hail', 'Precipitation', 'Rain', 'Snow']].astype(int)
+
+    types_lat_month_df['Most Events Type'] = types_lat_month_df[['Cold', 'Fog', 'Hail', 'Precipitation', 'Rain', 'Snow']].idxmax(axis=1)
+
+    types_lat_month_df['Most Events Count'] = types_lat_month_df[['Cold', 'Fog', 'Hail', 'Precipitation', 'Rain', 'Snow']].max(axis = 1)
+
+    types_lat_month_df['Most Events and Type'] = types_lat_month_df['Most Events Type'] + ', ' + types_lat_month_df['Most Events Count'].astype(str)
+    
+    selected_df = types_lat_month_df[['Month', 'Lat Decile Rank', 'Most Events Count', 'Most Events Type', 'Most Events and Type']]
+
+    pivot_df = pd.pivot_table(selected_df, columns = ['Month'], index = ['Lat Decile Rank'], values = ['Most Events and Type'], aggfunc=lambda x: ' '.join(x))
+
+    ordered_columns = [('Most Events and Type', 'Jan'), ('Most Events and Type', 'Feb'), ('Most Events and Type', 'Mar'), 
+                    ('Most Events and Type', 'Apr'), ('Most Events and Type', 'May'), ('Most Events and Type', 'Jun'),
+                    ('Most Events and Type', 'Jul'), ('Most Events and Type', 'Aug'), ('Most Events and Type', 'Sep'),
+                    ('Most Events and Type', 'Oct'), ('Most Events and Type', 'Nov'), ('Most Events and Type', 'Dec')]
+
+    pivot_df = pivot_df[ordered_columns]
+    pivot_df.columns = pivot_df.columns.map(': '.join)
+    pivot_df = pivot_df.reset_index()
+
+    
+
+    trace = dict(type = 'table', 
+            header = dict(values=list(pivot_df.columns), align = 'left'),
+            cells = dict(values = [
+                        pivot_df['Lat Decile Rank'], pivot_df['Most Events and Type: Jan'], pivot_df['Most Events and Type: Feb'], pivot_df['Most Events and Type: Mar'], 
+                        pivot_df['Most Events and Type: Apr'], pivot_df['Most Events and Type: May'], pivot_df['Most Events and Type: Jun'], pivot_df['Most Events and Type: Jul'], 
+                        pivot_df['Most Events and Type: Aug'], pivot_df['Most Events and Type: Sep'], pivot_df['Most Events and Type: Oct'],pivot_df['Most Events and Type: Nov'], 
+                        pivot_df['Most Events and Type: Dec']],
+                        align = 'center',
+                        fill = dict(color = ['rgb(245, 245, 245)', [color_matcher(val) for val in pivot_df['Most Events and Type: Jan']],
+                
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Feb']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Mar']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Apr']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: May']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Jun']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Jul']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Aug']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Sep']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Oct']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Nov']],
+                                    [color_matcher(val) for val in pivot_df['Most Events and Type: Dec']]
+                                    ]
+                                               
+                                   ))
+            
+            )
+            
+    trace['cells']['fill']
+    fig = go.Figure(data = [go.Table(
+        header = dict(values=list(pivot_df.columns),
+            align='left'),
+        cells = dict(values = [pivot_df['Lat Decile Rank'], pivot_df['Most Events and Type: Jan'], 
+                        pivot_df['Most Events and Type: Feb'], pivot_df['Most Events and Type: Mar'], pivot_df['Most Events and Type: Apr'],
+                        pivot_df['Most Events and Type: May'], pivot_df['Most Events and Type: Jun'], pivot_df['Most Events and Type: Jul'], 
+                        pivot_df['Most Events and Type: Aug'], pivot_df['Most Events and Type: Sep'], pivot_df['Most Events and Type: Oct'],
+                        pivot_df['Most Events and Type: Nov'], pivot_df['Most Events and Type: Dec']],
+                        align = 'center')),
+                                       
+            ])
+
+    fig = fig.add_traces(data = [trace])
+    fig.write_image("output_Graphs/most_common_events_by_latitude_by_month.jpeg", width = 1200, height = 800)
+
+
+
 
 def prob2_graph():
     severe_df = pd.read_csv(problem_2_3_csv)
@@ -42,5 +131,7 @@ def prob3_graph():
 
 
 if __name__ == "__main__":
+    prob1_graph()
+    prob2_graph()
     prob3_graph()
    
